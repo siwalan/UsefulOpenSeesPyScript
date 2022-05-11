9ef62f2a4e04901996ac635541e46a102a7e39e2
siwalan <59278902+siwalan@users.noreply.github.com>
2022-05-11 19:09:56 +0800

    Update nat2localStiff.py
    Add documentation

--- nat2localStiff.py
+++ nat2localStiff.py
@@ -1,3 +1,5 @@
+### Convert the output of ops.basicStiffness() to more common local stifness matrix. Expect a 3x3 input for 2D element.
+
 def nat2localStiff(Knat,L=1.0):    
     Tlocal = np.matrix([[-1, 0, 0, 1, 0, 0],[0, -1/L, 0, 0, 1/L,1],[0, -1/L, 1, 0, 1/L, 0]])
     return (np.transpose(Tlocal) @ Knat @ Tlocal)
